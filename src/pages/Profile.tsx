07c35a5f62e5251c05c7d2166257185600c61421
Daffadon <daffaputranarendra9@gmail.com>
2024-07-17 21:25:01 +0700

    feat: add getprofile function

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,46 @@
 import UserLayout from '../components/layout/UserLayout';
 import dummypict from '../assets/profpicdummy.png';
 import { optionsSelection } from '../data/page/analysis/options';
+import { useEffect, useState } from 'react';
+import { axiosClient } from '../lib/axios-client';
+import { errorNotification } from '../components/toast/notification';
+import { ToastContainer } from 'react-toastify';
+interface ProfileProps {
+  fullname: string;
+  email: string;
+}
 const Profile = () => {
+  const [profile, setProfile] = useState<ProfileProps>()
+  const getProfile = async () => {
+    try {
+      const { data } = await axiosClient.get('/profile', {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      if (data) {
+        setProfile(data.data)
+      }
+    } catch (error) {
+      errorNotification("Failed to fetch UserData")
+    }
+  }
+  useEffect(() => {
+    getProfile()
+  }, [])
   return (
     <UserLayout>
       <h1 className="text-center py-5 font-bold text-2xl">Welcome to Your Profile Page!</h1>
       <div className="flex justify-center items-center">
         <img src={dummypict} alt="" className="rounded-full w-40 h-40 object-cover" />
       </div>
-      <h1 className="text-center text-2xl pt-5">Daffa Putra Narendra</h1>
-      <h2 className="text-center text-lg">daffadummy@gmail.com</h2>
-      <h3 className="text-center text-lg">Doctor: dr. Achmad </h3>
-      <h6 className="text-center cursor-pointer hover:text-[#1565c0] duration-300">
+      <h1 className="text-center text-2xl pt-5">{profile?.fullname}</h1>
+      <h2 className="text-center text-lg">{profile?.email}</h2>
+      {/* <h3 className="text-center text-lg">Doctor: dr. Achmad </h3> */}
+      {/* <h6 className="text-center cursor-pointer hover:text-[#1565c0] duration-300">
         Change Your password
-      </h6>
-      {/* ubah kata santi */}
-      {/* history */}
-      <h3 className="text-center mt-5">History</h3>
+      </h6> */}
+      <h3 className="text-center mt-5 font-bold text-xl">History</h3>
       <div className="flex justify-around items-center mt-3">
         <table className="border-2 border-black rounded-md p-2 w-1/2">
           <thead>
@@ -26,9 +50,9 @@ const Profile = () => {
             </tr>
           </thead>
           <tbody>
-            {optionsSelection.map((option) => {
+            {optionsSelection.map((option, index) => {
               return (
-                <tr className="border border-black">
+                <tr key={index} className="border border-black">
                   <td className="w-1/2 border border-black text-center py-1">{option.label}</td>
                   <td className="w-1/2 border border-black text-center py-1">
                     {option.analyzed ? 'V' : 'X'}
@@ -39,11 +63,7 @@ const Profile = () => {
           </tbody>
         </table>
       </div>
-      {/* Data belum dianalisis */}
-      {/* Data telah dianalisis */}
-
-      {/* Admin // suster/perawat */}
-      {/* Doctor */}
+      <ToastContainer />
     </UserLayout>
   );
 };
