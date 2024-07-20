98fa4cee401a6cd364d3d30c21d618c458b2feb9
Daffadon <daffaputranarendra9@gmail.com>
2024-07-20 20:17:18 +0700

    refactor: change interface properties

--- src/data/dto/fullDataJson.ts
+++ src/data/dto/fullDataJson.ts
@@ -1,26 +1,22 @@
 interface JsonDatatype {
-  value: {
-    name: string;
-    date: string;
-    prediction: { N: number; S: number; V: number; F: number; Q: number };
-    feature: string;
-    runtime: number;
-    ecgplot: number[];
-    annotation: {
-      N: never[] | number[][];
-      S: never[] | number[][];
-      V: never[] | number[][];
-      F: never[] | number[][];
-      Q: never[] | number[][];
-    };
-    sample_plot: {
-      N: number[] | number[][];
-      S: number[] | number[][];
-      V: number[] | number[][];
-      F: number[] | number[][];
-      Q: number[] | number[][];
-    };
+  name: string;
+  date: string;
+  prediction: { N: number; S: number; V: number; F: number; Q: number };
+  feature: string;
+  runtime: number;
+  ecgplot: number[];
+  annotation: {
+    N: never[] | number[][];
+    S: never[] | number[][];
+    V: never[] | number[][];
+    F: never[] | number[][];
+    Q: never[] | number[][];
   };
-  comment: string[];
-  label: string;
-}
+  sample_plot: {
+    N: number[] | number[][];
+    S: number[] | number[][];
+    V: number[] | number[][];
+    F: number[] | number[][];
+    Q: number[] | number[][];
+  };
+} 
