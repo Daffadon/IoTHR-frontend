61adf89e4ddda75bc04ec405e78dd2f8b97a2391
Daffadon <daffaputranarendra9@gmail.com>
2024-07-20 20:16:57 +0700

    refactor: remove console log error

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -15,7 +15,6 @@ axiosClient.interceptors.response.use(
     return response;
   },
   (error) => {
-    console.log(error);
     const { response } = error;
     if (response.status === 401) {
       localStorage.removeItem('ACCESS_TOKEN');
