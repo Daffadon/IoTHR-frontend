82efd0897c1e2e62bfcf9cb17a6151108ca2c7e0
Daffadon <daffaputranarendra9@gmail.com>
2024-07-22 15:01:38 +0700

    feat: add react countdown for recording and auto predict

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -1,9 +1,11 @@
 
-import { useState, FormEvent } from 'react';
+import { useState, FormEvent, useEffect } from 'react';
 import UserLayout from '../components/layout/UserLayout';
 import { axiosClient } from '../lib/axios-client';
 import { ToastContainer } from 'react-toastify';
 import { errorNotification, successNotification } from '../components/toast/notification';
+import Countdown, { CountdownRenderProps } from 'react-countdown';
+import { useNavigate } from 'react-router-dom';
 
 // Define types
 type DeviceType = "Sense" | "H10";
@@ -30,12 +32,29 @@ const Record = () => {
     status: false,
     topicId: ""
   })
+  const navigate = useNavigate()
   const [device, setDevice] = useState<Device>();
-  const [record, setRecord] = useState<boolean | undefined>(undefined)
   const [loading, setLoading] = useState<boolean>(false)
+  const [onRecord, setOnRecord] = useState<boolean>(false)
+  const [isComplete, setIsComplete] = useState<boolean>(false)
+  const [firstTime, setFirstTime] = useState<boolean>(true)
+
+  const predict = async () => {
+    try {
+      await axiosClient.patch('/topic/prediction', {
+        topicId: form.topicId
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   const sendECGData = async (data: unknown[]) => {
     try {
+      if (firstTime) {
+        setOnRecord(true)
+        setFirstTime(false)
+      }
       await axiosClient.patch('/topic/ecg', {
         topicId: form.topicId,
         ECGplot: data,
@@ -70,14 +89,6 @@ const Record = () => {
       await device.character.writeValue(new Uint8Array(BytesToSend));
     }
     successNotification("Recording Started")
-  }
-
-  const stopRecording = async (device: Device | undefined, stream: any) => {
-    const off = new Uint8Array([0x03, stream.id]);
-    if (device?.character) {
-      await device?.character.writeValue(off);
-    }
-    errorNotification("Recording Stopped")
   }
 
   const onDisconnected = () => {
@@ -168,65 +179,105 @@ const Record = () => {
     }
   };
 
+  const renderer = ({ hours, minutes, seconds, completed }: CountdownRenderProps) => {
+    if (completed) {
+      setIsComplete(true)
+      return <span className='text-white text-4xl'>Time's up!</span>;
+    } else {
+      return (
+        <div className='text-white text-4xl'>
+          <span>{hours.toString().padStart(2, '0')}</span>:
+          <span>{minutes.toString().padStart(2, '0')}</span>:
+          <span>{seconds.toString().padStart(2, '0')}</span>
+        </div>
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (isComplete) {
+      const stopRecording = async (device: Device | undefined, stream: any) => {
+        const off = new Uint8Array([0x03, stream.id]);
+        if (device?.character) {
+          await device?.character.writeValue(off);
+        }
+        await predict()
+        errorNotification("Recording Stopped")
+        setForm({
+          topicName: "",
+          status: false,
+          topicId: ""
+        })
+        setOnRecord(false)
+        navigate('/history')
+      }
+
+      stopRecording(device, stream.ECG)
+    }
+  }, [isComplete])
+
   return (
-    <UserLayout>
-      {form.status ?
-        (loading ?
-          (<div className='flex h-screen justify-center items-center'>
-            <p className='font-bold text-xl'>Connecting...</p >
-          </div>)
-          :
-          (<div className='flex justify-center items-center h-screen flex-col'>
-            <p className='font-bold text-xl mb-3'>{form.topicName}</p >
-            {!device && (
-              <button onClick={addDevice} className='bg-blue-600 px-4 py-2 text-white font-bold rounded-lg'>Add Device</button>
-            )}
-            {device && (
-              <div className='flex flex-col justify-center items-center'>
-                <p className='font-semibold text-xl'>Sensor {device?.device?.name}</p>
-                <div className='flex gap-3 mt-4'>
-                  <button
-                    disabled={record}
-                    className='bg-blue-600 px-4 py-2 text-white font-bold rounded-lg' onClick={() => {
-                      setRecord(true)
-                      startRecording(device, stream.ECG)
-                    }}> Record ECG
-                  </button>
-                  <button
-                    disabled={!record}
-                    className='bg-red-600  px-4 py-2 text-white font-bold rounded-lg' onClick={() => {
-                      setRecord(false)
-                      stopRecording(device, stream.ECG)
-                    }}>Stop Record
-                  </button>
+    <>
+      {onRecord && (
+        <div className='absolute h-screen w-full bg-black z-10 opacity-95 flex justify-center items-center'>
+          <Countdown className='z-10' date={Date.now() + 60000}
+            renderer={renderer}>
+          </Countdown>
+        </div >
+      )}
+      <UserLayout record={onRecord}>
+        {form.status ?
+          (loading ?
+            (<div className='flex h-screen justify-center items-center'>
+              <p className='font-bold text-xl'>Connecting...</p >
+            </div>)
+            :
+            (<div className='flex justify-center items-center h-screen flex-col'>
+              <p className='font-bold text-xl mb-3'>{form.topicName}</p >
+              {!device && (
+                <button onClick={addDevice} className='bg-blue-600 px-4 py-2 text-white font-bold rounded-lg'>Add Device</button>
+              )}
+              {device && (
+                <div className='flex flex-col justify-center items-center'>
+                  <p className='font-semibold text-xl'>Sensor {device?.device?.name}</p>
+                  <div className='flex gap-3 mt-4'>
+                    <button
+                      className='bg-blue-600 px-4 py-2 text-white font-bold rounded-lg' onClick={() => {
+                        startRecording(device, stream.ECG)
+                      }}> Record ECG
+                    </button>
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>)
-        ) :
-        <form onSubmit={createTopic} className='flex flex-col justify-center items-center h-screen' >
-          <label htmlFor="username" className="text-lg">
-            Topic Name
-          </label>
-          <input
-            type="text"
-            name="email"
-            id="email"
-            value={form.topicName}
-            onChange={(e) => {
-              setForm((prev) => ({ ...prev, topicName: e.target.value }));
-            }}
-            className="rounded text-black mt-2 focus:outline-none px-2 py-1"
-          />
-          <button
-            type="submit"
-            className="bg-black mt-5 px-9 py-1 rounded hover:bg-white hover:text-black duration-300 text-white">
-            Create
-          </button>
-        </form>}
-      <ToastContainer />
-    </UserLayout >
+              )}
+            </div>)
+          ) :
+          <form onSubmit={createTopic} className='flex flex-col justify-center items-center h-screen' >
+            <label htmlFor="username" className="text-lg">
+              Topic Name
+            </label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={form.topicName}
+              onChange={(e) => {
+                setForm((prev) => ({ ...prev, topicName: e.target.value }));
+              }}
+              className="rounded text-black mt-2 focus:outline-none px-2 py-1"
+            />
+            <button
+              type="submit"
+              className="bg-black mt-5 px-9 py-1 rounded hover:bg-white hover:text-black duration-300 text-white hover:border-black hover:border">
+              Create
+            </button>
+          </form>}
+        <ToastContainer className={"z-10"} />
+      </UserLayout >
+    </>
   );
 };
+
+
+
 
 export default Record;
