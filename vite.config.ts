763fed9aaeddde97c099f525cdc64247a7cb7c8a
Daffadon <daffaputranarendra9@gmail.com>
2024-08-19 08:01:23 +0700

    build: change build strategy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    cssMinify: true,
+    ssrManifest: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'flowbite', 'flowbite-react']
+        }
+      }
+    }
+  }
+});
