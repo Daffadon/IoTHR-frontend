6eee782b98e1d38ebaacaebafaddd3b9915b833c
Daffadon <daffaputranarendra9@gmail.com>
2024-08-23 15:55:42 +0700

    refactor: implement loading and error handling

--- src/pages/doctor/Analyze.tsx
+++ src/pages/doctor/Analyze.tsx
@@ -10,6 +10,9 @@ import { IoArrowBackCircleOutline } from "react-icons/io5";
 import { Button, Modal, Textarea, ToggleSwitch } from "flowbite-react";
 import { toggleSwitchTheme } from "../../styles/toggleSwitch";
 import { CiSquareRemove } from "react-icons/ci";
+import Loading from "../../components/loading/Loading";
+import { prediction } from "../../api/prediction/prediction";
+import { deleteTopic } from "../../api/topic/topic";
 
 
 const Analyze = () => {
@@ -30,6 +33,7 @@ const Analyze = () => {
     Q: []
   });
   const [modal, setModal] = useState(false)
+  const [loading, setLoading] = useState<boolean>(false)
   const [deleteModal, setDeleteModal] = useState({
     show: false,
     commentIndex: -1
@@ -80,8 +84,8 @@ const Analyze = () => {
         setPredictionOption(newObj)
         setSelectedFeature(newObj[0])
       }
-    } catch (error) {
-      errorNotification("Failed to fetch Prediction")
+    } catch (error: any) {
+      errorNotification(error.response.data.error)
     }
   }
 
@@ -92,19 +96,37 @@ const Analyze = () => {
         setPredictionData(data.data)
       }
     } catch (error) {
-      errorNotification("Failed to fetch Prediction")
+      errorNotification("Failed to get Prediction")
     }
   }
 
   const getTopic = async () => {
     try {
       const { data } = await axiosClient.get('/doctor/topic/' + topicId)
+      if (!data.topic.ecg_plot) {
+        await prediction(topicId)
+        window.location.reload()
+      }
       if (data) {
         setOptionsSelection(data.topic)
         getPrediction(data.topic.topic.id)
       }
-    } catch (error) {
-      errorNotification("Failed to fetch Topic")
+    } catch (error: any) {
+      errorNotification(error.response.data.error + "Trying to generate from recorded data")
+      try {
+        await prediction(topicId)
+        window.location.reload()
+      } catch (error: any) {
+        if (error.response.status == 404) {
+          errorNotification(error.response.data.error + ". Deleting the topic")
+          try {
+            await deleteTopic(topicId)
+            setTimeout(() => {
+              window.location.reload()
+            }, 2500)
+          } catch (error) { }
+        }
+      }
     }
   }
 
@@ -115,118 +137,126 @@ const Analyze = () => {
       const { N, S, V, F, Q } = predictionData?.sample_plot;
       setSamplePlot({ N, S, V, F, Q })
     }
+    setLoading(false)
   }, [predictionData])
 
   useEffect(() => {
     if (selectedFeature) {
+      setLoading(true)
       getPredictionById(selectedFeature.value)
     }
   }, [selectedFeature])
 
   useEffect(() => {
+    setLoading(true)
     getTopic()
   }, [])
 
 
   return (
-    <DoctorLayout>
-      <h1 className="py-2 font-bold text-2xl text-center">Heart Rate Graphic</h1>
-      <IoArrowBackCircleOutline className="absolute text-5xl left-10 top-16 cursor-pointer hover:text-blue-500 duration-150" onClick={() => { navigate(-1) }} />
-      <ToggleSwitch checked={optionsSelection?.topic.analyzed ?? false} onChange={switchHandler} label={`${optionsSelection?.topic.analyzed ? "Analyzed" : "Not Analyzed"}`} className="absolute right-10 top-5" theme={toggleSwitchTheme} sizing={"md"} color="blue" />
-      <div className="flex gap-4 justify-center items-center py-5 focus:border-none">
-        <Select
-          isSearchable={false}
-          value={selectedFeature}
-          onChange={handleFeatureSelectChange}
-          options={predictionOption}
-          className="w-1/3 border-black focus:outline-none focus-within::border-black max-h-[20vh] text-black"
-        />
-      </div>
-      {optionsSelection && <p className='text-center font-semibold'>Recorded At: <span className='text-blue-600'>{optionsSelection?.topic.date}</span></p>}
-      {optionsSelection && <p className='text-center font-semibold'>Recording Time: <span className='text-blue-600'>{optionsSelection?.topic.recordTime}</span></p>}
-      <div style={{ width: '100%', height: '400px' }}>
-        {optionSeries &&
-          <ReactECharts option={optionSeries} style={{ width: '100%', height: '100%' }} notMerge={true} />
-        }
-      </div>
-      <h1 className='text-center text-lg font-semibold mt-10'>Sample Plot</h1>
-      <div className='flex justify-center items-center w-full flex-wrap'>
-        {samplePlot?.N.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.N, "N")} className='w-4/12' />
-        )}
-        {samplePlot?.S.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.S, "S")} className='w-4/12' />
-        )}
-        {samplePlot.V.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.V, "V")} className='w-4/12' />
-        )}
-        {samplePlot.F.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.F, "F")} className='w-4/12' />
-        )}
-        {samplePlot.Q.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.Q, "Q")} className='w-4/12' />
-        )}
-      </div>
-      <div className='flex justify-center'>
-        <div style={{ width: '40%', height: '400px' }}>
-          {optionBarSeries &&
-            <ReactECharts option={optionBarSeries} style={{ width: "100%", height: "100%" }} />
+    loading ? <Loading /> :
+      <DoctorLayout>
+        <h1 className="py-2 font-bold text-2xl text-center">Heart Rate Graphic</h1>
+        <IoArrowBackCircleOutline className="absolute text-5xl left-10 top-16 cursor-pointer hover:text-blue-500 duration-150" onClick={() => { navigate(-1) }} />
+        <ToggleSwitch checked={optionsSelection?.topic.analyzed ?? false} onChange={switchHandler} label={`${optionsSelection?.topic.analyzed ? "Analyzed" : "Not Analyzed"}`} className="absolute right-10 top-5" theme={toggleSwitchTheme} sizing={"md"} color="blue" />
+        <div className="flex gap-4 justify-center items-center py-5 focus:border-none">
+          <Select
+            isSearchable={false}
+            value={selectedFeature}
+            onChange={handleFeatureSelectChange}
+            options={predictionOption}
+            className="w-1/3 border-black focus:outline-none focus-within::border-black max-h-[20vh] text-black"
+          />
+        </div>
+        {optionsSelection && <p className='text-center font-semibold'>Recorded At: <span className='text-blue-600'>{optionsSelection?.topic.date}</span></p>}
+        {optionsSelection && <p className='text-center font-semibold'>Recording Time: <span className='text-blue-600'>{optionsSelection?.topic.recordTime}</span></p>}
+        <div style={{ width: '100%', height: '400px' }}>
+          {optionSeries &&
+            <ReactECharts option={optionSeries} style={{ width: '100%', height: '100%' }} notMerge={true} />
           }
         </div>
-      </div>
-      <div className="pb-5">
-        <button onClick={() => { setModal(true) }} className="bg-black mt-5 px-3 py-2 text-white hover:bg-blue-500 rounded duration-300 ml-10">Add Analysis</button>
-        <p className="mt-4 ml-10 text-xl font-semibold">Analysis</p>
-        {optionsSelection?.topic.analysis == null || optionsSelection?.topic.analysis.length === 0 ?
-          <p className="ml-10">No Analysis yet</p>
-          : optionsSelection?.topic.analysis.map((doctor, index) => {
-            return <div key={index}>
-              <p className="ml-10 mt-2 text-lg font-semibold">From Doctor {doctor.doctorName}</p>
-              <ul className="ml-10">
-                {doctor.comment.map((comment, index) => {
-                  return <li key={index} className="flex mt-2 items-center">
-                    <button onClick={() => setDeleteModal({
-                      show: true,
-                      commentIndex: index
-                    })}>
-                      <CiSquareRemove className="text-3xl rounded mr-2 hover:bg-red-500 duration-150" />
-                    </button>
-                    <p>"{comment}"</p>
-                  </li>
-                })}
-              </ul>
-            </div>
-          })}
-      </div>
-      <Modal show={deleteModal.show} size="md" onClose={() => { setDeleteModal({ show: false, commentIndex: -1 }) }} popup>
-        <Modal.Header />
-        <Modal.Body>
-          <h1 className="text-center">Are you sure want to delete this comment?</h1>
-          <form className="flex items-center justify-center gap-5" onSubmit={deleteCommentHandler}>
-            <Button type="submit" className="mt-4 bg-red-500">Delete</Button>
-            <Button onClick={() => setDeleteModal({ show: false, commentIndex: -1 })} className="mt-4">Cancel</Button>
-          </form>
-        </Modal.Body>
-      </Modal>
-      <Modal show={modal} size="md" onClose={() => { setModal(false) }} popup>
-        <Modal.Header />
-        <Modal.Body>
-          <h1>Add your analysis here</h1>
-          <form onSubmit={addCommentHandler}>
-            <Textarea
-              id="analysis-comment"
-              placeholder="Your analysis here"
-              value={comment}
-              onChange={(event) => setComment(event.target.value)}
-              required
-              className="mt-4 resize-none focus:outline-none focus:border-none focus:right-0"
-              rows={5}
-            />
-            <Button type="submit" className="mt-4">Submit</Button>
-          </form>
-        </Modal.Body>
-      </Modal>
-    </DoctorLayout>
+        {samplePlot?.N.length > 0 && (
+          <h1 className='text-center text-lg font-semibold pb-4'>Sample Plot</h1>
+        )}
+        <div className='flex justify-center items-center w-full flex-wrap'>
+          {samplePlot?.N.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.N, "N")} className='w-4/12' />
+          )}
+          {samplePlot?.S.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.S, "S")} className='w-4/12' />
+          )}
+          {samplePlot.V.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.V, "V")} className='w-4/12' />
+          )}
+          {samplePlot.F.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.F, "F")} className='w-4/12' />
+          )}
+          {samplePlot.Q.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.Q, "Q")} className='w-4/12' />
+          )}
+        </div>
+        <div className='flex justify-center'>
+          <div style={{ width: '40%', height: '400px' }}>
+            {optionBarSeries &&
+              <ReactECharts option={optionBarSeries} style={{ width: "100%", height: "100%" }} />
+            }
+          </div>
+        </div>
+        <div className="pb-5">
+          <button onClick={() => { setModal(true) }} className="bg-black mt-5 px-3 py-2 text-white hover:bg-blue-500 rounded duration-300 ml-10">Add Analysis</button>
+          {optionsSelection?.topic.analysis == null || optionsSelection?.topic.analysis.length === 0 ?
+            <>
+              <p className="mt-4 ml-10 text-xl font-semibold">Analysis</p>
+              <p className="ml-10">No Analysis yet</p>
+            </>
+            : optionsSelection?.topic.analysis.map((doctor, index) => {
+              return <div key={index}>
+                <p className="ml-10 mt-2 text-lg font-semibold">From Doctor {doctor.doctorName}</p>
+                <ul className="ml-10">
+                  {doctor.comment.map((comment, index) => {
+                    return <li key={index} className="flex mt-2 items-center">
+                      <button onClick={() => setDeleteModal({
+                        show: true,
+                        commentIndex: index
+                      })}>
+                        <CiSquareRemove className="text-3xl rounded mr-2 hover:bg-red-500 duration-150" />
+                      </button>
+                      <p>"{comment}"</p>
+                    </li>
+                  })}
+                </ul>
+              </div>
+            })}
+        </div>
+        <Modal show={deleteModal.show} size="md" onClose={() => { setDeleteModal({ show: false, commentIndex: -1 }) }} popup>
+          <Modal.Header />
+          <Modal.Body>
+            <h1 className="text-center">Are you sure want to delete this comment?</h1>
+            <form className="flex items-center justify-center gap-5" onSubmit={deleteCommentHandler}>
+              <Button type="submit" className="mt-4 bg-red-500">Delete</Button>
+              <Button onClick={() => setDeleteModal({ show: false, commentIndex: -1 })} className="mt-4">Cancel</Button>
+            </form>
+          </Modal.Body>
+        </Modal>
+        <Modal show={modal} size="md" onClose={() => { setModal(false) }} popup>
+          <Modal.Header />
+          <Modal.Body>
+            <h1>Add your analysis here</h1>
+            <form onSubmit={addCommentHandler}>
+              <Textarea
+                id="analysis-comment"
+                placeholder="Your analysis here"
+                value={comment}
+                onChange={(event) => setComment(event.target.value)}
+                required
+                className="mt-4 resize-none focus:outline-none focus:border-none focus:right-0"
+                rows={5}
+              />
+              <Button type="submit" className="mt-4">Submit</Button>
+            </form>
+          </Modal.Body>
+        </Modal>
+      </DoctorLayout>
   );
 }
 
--- src/pages/user/History.tsx
+++ src/pages/user/History.tsx
@@ -7,6 +7,9 @@ import { setChartBarOption, setChartLineOption, setPlotOption } from '../../feat
 import { axiosClient } from '../../lib/axios-client';
 import { errorNotification } from '../../components/toast/notification';
 import { HistoryProps } from '../../data/dto/profile';
+import { prediction } from '../../api/prediction/prediction';
+import { deleteTopic } from '../../api/topic/topic';
+import Loading from '../../components/loading/Loading';
 
 
 const History = () => {
@@ -26,6 +29,8 @@ const History = () => {
     F: [],
     Q: []
   });
+
+  const [loading, setLoading] = useState<boolean>(false)
   const handleSelectChange = (selectedOption: SingleValue<SelectionOption>) => {
     setSelected(selectedOption!);
   };
@@ -44,8 +49,8 @@ const History = () => {
         setHistory(newObj)
         setSelected(newObj[0])
       }
-    } catch (error) {
-      errorNotification("Failed to fetch History")
+    } catch (error: any) {
+      errorNotification(error.response.data.error)
     }
   }
   const getPrediction = async (topicId: string) => {
@@ -58,8 +63,10 @@ const History = () => {
         setPredictionOption(newObj)
         setSelectedFeature(newObj[0])
       }
-    } catch (error) {
-      errorNotification("Failed to fetch Prediction")
+    } catch (error: any) {
+      errorNotification(error.response.data.error)
+      await prediction(topicId)
+      window.location.reload()
     }
   }
 
@@ -70,19 +77,40 @@ const History = () => {
         setPredictionData(data.data)
       }
     } catch (error) {
-      errorNotification("Failed to fetch Prediction")
+      errorNotification("Failed to get Prediction")
     }
   }
 
   const getTopic = async () => {
     try {
       const { data } = await axiosClient.get('/topic/' + selected?.value)
+      if (!data.topic.ecg_plot) {
+        await prediction(selected?.value)
+        window.location.reload()
+      }
       if (data) {
         setOptionsSelection(data.topic)
         getPrediction(data.topic.topic.id)
       }
-    } catch (error) {
-      errorNotification("Failed to fetch Topic")
+    } catch (error: any) {
+      if (error.response.status == 404) {
+        errorNotification(error.response.data.error + "Trying to generate from recorded data")
+        try {
+          await prediction(selected?.value)
+          window.location.reload()
+        } catch (error: any) {
+          if (error.response.status == 404) {
+            errorNotification(error.response.data.error + ". Deleting the topic")
+            try {
+              await deleteTopic(selected?.value)
+              setTimeout(() => {
+                window.location.reload()
+              }, 2500)
+            } catch (error) { }
+          }
+        }
+      }
+      errorNotification(error.response.data.error)
     }
   }
 
@@ -93,92 +121,101 @@ const History = () => {
       const { N, S, V, F, Q } = predictionData?.sample_plot;
       setSamplePlot({ N, S, V, F, Q })
     }
+    setLoading(false)
   }, [predictionData])
 
   useEffect(() => {
     if (selectedFeature) {
+      setLoading(true)
       getPredictionById(selectedFeature.value)
     }
   }, [selectedFeature])
 
   useEffect(() => {
     if (selected) {
+      setLoading(true)
       getTopic()
     }
   }, [selected])
 
   useEffect(() => {
+    setLoading(true);
     getHistory()
   }, [])
 
 
   return (
-    <UserLayout>
-      <h1 className="py-2 font-bold text-2xl text-center">Heart Rate Graphic</h1>
-      <div className="flex gap-4 justify-center items-center py-5 focus:border-none">
-        <Select
-          isSearchable={false}
-          value={selected}
-          onChange={handleSelectChange}
-          options={history}
-          className="w-1/3 border-black focus:outline-none focus-within::border-black max-h-[20vh] text-black"
-        />
-        <Select
-          isSearchable={false}
-          value={selectedFeature}
-          onChange={handleFeatureSelectChange}
-          options={predictionOption}
-          className="w-1/3 border-black focus:outline-none focus-within::border-black max-h-[20vh] text-black"
-        />
-      </div>
-      {optionsSelection && <p className='text-center font-semibold'>Recorded At: <span className='text-blue-600'>{optionsSelection?.topic.date}</span></p>}
-      {optionsSelection && <p className='text-center font-semibold'>Recording Time: <span className='text-blue-600'>{optionsSelection?.topic.recordTime}</span></p>}
-      <div style={{ width: '100%', height: '400px' }}>
-        {optionSeries &&
-          <ReactECharts option={optionSeries} style={{ width: '100%', height: '100%' }} notMerge={true} />
-        }
-      </div>
-      <h1 className='text-center text-lg font-semibold pb-4'>Sample Plot</h1>
-      <div className='flex justify-center items-center w-full flex-wrap px-3'>
-        {samplePlot?.N.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.N, "N")} className='w-4/12' />
-        )}
-        {samplePlot?.S.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.S, "S")} className='w-4/12' />
-        )}
-        {samplePlot.V.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.V, "V")} className='w-4/12' />
-        )}
-        {samplePlot.F.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.F, "F")} className='w-4/12' />
-        )}
-        {samplePlot.Q.length > 0 && (
-          <ReactECharts option={setPlotOption(samplePlot?.Q, "Q")} className='w-4/12' />
-        )}
-      </div>
-      <div className='flex justify-center'>
-        <div style={{ width: '40%', height: '400px' }}>
-          {optionBarSeries &&
-            <ReactECharts option={optionBarSeries} style={{ width: "100%", height: "100%" }} />
+    loading ? <Loading /> :
+      <UserLayout>
+        <h1 className="py-2 font-bold text-2xl text-center">Heart Rate Graphic</h1>
+        <div className="flex gap-4 justify-center items-center py-5 focus:border-none">
+          <Select
+            isSearchable={false}
+            value={selected}
+            onChange={handleSelectChange}
+            options={history}
+            className="w-1/3 border-black focus:outline-none focus-within::border-black max-h-[20vh] text-black"
+          />
+          <Select
+            isSearchable={false}
+            value={selectedFeature}
+            onChange={handleFeatureSelectChange}
+            options={predictionOption}
+            className="w-1/3 border-black focus:outline-none focus-within::border-black max-h-[20vh] text-black"
+          />
+        </div>
+        {optionsSelection && <p className='text-center font-semibold'>Recorded At: <span className='text-blue-600'>{optionsSelection?.topic.date}</span></p>}
+        {optionsSelection && <p className='text-center font-semibold'>Recording Time: <span className='text-blue-600'>{optionsSelection?.topic.recordTime}</span></p>}
+        <div style={{ width: '100%', height: '400px' }}>
+          {optionSeries &&
+            <ReactECharts option={optionSeries} style={{ width: '100%', height: '100%' }} notMerge={true} />
           }
         </div>
-      </div>
-      <div className='mb-5'>
-        <p className="mt-4 ml-10 text-xl font-semibold">Analysis</p>
-        {optionsSelection?.topic.analysis == null || optionsSelection?.topic.analysis.length === 0 ?
-          <p className="ml-10">No Analysis yet</p>
-          : optionsSelection?.topic.analysis.map((doctor, index) => {
-            return <>
-              <p key={index} className="ml-10 mt-2 text-md font-semibold">From Doctor {doctor.doctorName}</p>
-              <ul className="ml-10">
-                {doctor.comment.map((comment, index) => {
-                  return <li key={index} className="ml-5">"{comment}"</li>
-                })}
-              </ul>
-            </>
-          })}
-      </div>
-    </UserLayout>
+        {samplePlot?.N.length > 0 && (
+          <h1 className='text-center text-lg font-semibold pb-4'>Sample Plot</h1>
+        )}
+        <div className='flex justify-center items-center w-full flex-wrap px-3'>
+          {samplePlot?.N.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.N, "N")} className='w-4/12' />
+          )}
+          {samplePlot?.S.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.S, "S")} className='w-4/12' />
+          )}
+          {samplePlot.V.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.V, "V")} className='w-4/12' />
+          )}
+          {samplePlot.F.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.F, "F")} className='w-4/12' />
+          )}
+          {samplePlot.Q.length > 0 && (
+            <ReactECharts option={setPlotOption(samplePlot?.Q, "Q")} className='w-4/12' />
+          )}
+        </div>
+        <div className='flex justify-center'>
+          <div style={{ width: '40%', height: '400px' }}>
+            {optionBarSeries &&
+              <ReactECharts option={optionBarSeries} style={{ width: "100%", height: "100%" }} />
+            }
+          </div>
+        </div>
+        {optionsSelection != null && (
+          <div className='mb-5'>
+            <p className="mt-4 ml-10 text-xl font-semibold">Analysis</p>
+            {optionsSelection?.topic.analysis == null || optionsSelection?.topic.analysis.length === 0 ?
+              <p className="ml-10">No Analysis yet</p>
+              : optionsSelection?.topic.analysis.map((doctor, index) => {
+                return <>
+                  <p key={index} className="ml-10 mt-2 text-md font-semibold">From Doctor {doctor.doctorName}</p>
+                  <ul className="ml-10">
+                    {doctor.comment.map((comment, index) => {
+                      return <li key={index} className="ml-5">"{comment}"</li>
+                    })}
+                  </ul>
+                </>
+              })}
+          </div>
+        )}
+      </UserLayout>
   );
 };
 
