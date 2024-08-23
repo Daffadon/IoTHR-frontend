00a85b7adff3106f63dd771285383d8ac7828970
Daffadon <daffaputranarendra9@gmail.com>
2024-08-23 15:57:14 +0700

    refactor: implement loading

--- src/pages/doctor/Patient.tsx
+++ src/pages/doctor/Patient.tsx
@@ -4,6 +4,7 @@ import { axiosClient } from "../../lib/axios-client";
 import { errorNotification } from "../../components/toast/notification";
 import { calculateAge } from "../../utils/calcualteAge";
 import { Link } from "react-router-dom";
+import Loading from "../../components/loading/Loading";
 
 interface User {
   id: string
@@ -14,52 +15,56 @@ interface User {
 
 const Patient = () => {
   const [users, setUsers] = useState<User[]>();
+  const [loading, setLoading] = useState<boolean>(false)
 
   const getUsers = async () => {
     try {
+      setLoading(true);
       const { data } = await axiosClient.get("/doctor/user");
       setUsers(data.data);
     } catch (error: any) {
       errorNotification(error.message);
+    } finally {
+      setLoading(false)
     }
   }
   useEffect(() => {
     getUsers()
   }, [])
   return (
-    <DoctorLayout>
-      <h1 className="text-center text-2xl font-bold py-5">Patient List</h1>
-      {/* search feature */}
-      <div className="flex justify-around items-center mt-3">
-        <table className="rounded-md p-2 w-3/4">
-          <thead>
-            <tr>
-              <th className="w-1/12 py-2">No</th>
-              <th className="w-1/4 py-2">Full Name</th>
-              <th className="w-1/4 py-2">Birth Date</th>
-              <th className="w-1/4 py-2">Age</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users && users.map((user, index) => {
-              return (
-                <tr key={user.id} className={`text-white mt-2 ${index % 2 == 0 ? "bg-blue-600" : "bg-blue-500"}`}>
-                  <td className="w-1/12 text-center py-3">{index + 1}</td>
-                  <td className="w-1/4 text-center py-3">{user.fullname}</td>
-                  <td className="w-1/4 text-center py-3">{user.birthDate}</td>
-                  <td className="w-1/4 text-center py-3">{calculateAge(user.birthDate)} Years old</td>
-                  <td className="w-1/4 text-center py-3">
-                    <Link to={`/patient/${user.id}`}>
-                      <button className="px-3 py-2 bg-white rounded text-black hover:scale-95text-sm font-semibold hover:text-white hover:bg-black duration-150">More Detail</button>
-                    </Link>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </DoctorLayout>
+    loading ? <Loading /> :
+      <DoctorLayout>
+        <h1 className="text-center text-2xl font-bold py-5">Patient List</h1>
+        <div className="flex justify-around items-center mt-3">
+          <table className="rounded-md p-2 w-3/4">
+            <thead>
+              <tr>
+                <th className="w-1/12 py-2">No</th>
+                <th className="w-1/4 py-2">Full Name</th>
+                <th className="w-1/4 py-2">Birth Date</th>
+                <th className="w-1/4 py-2">Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users && users.map((user, index) => {
+                return (
+                  <tr key={user.id} className={`text-white mt-2 ${index % 2 == 0 ? "bg-blue-600" : "bg-blue-500"}`}>
+                    <td className="w-1/12 text-center py-3">{index + 1}</td>
+                    <td className="w-1/4 text-center py-3">{user.fullname}</td>
+                    <td className="w-1/4 text-center py-3">{user.birthDate}</td>
+                    <td className="w-1/4 text-center py-3">{calculateAge(user.birthDate)} Years old</td>
+                    <td className="w-1/4 text-center py-3">
+                      <Link to={`/patient/${user.id}`}>
+                        <button className="px-3 py-2 bg-white rounded text-black hover:scale-95text-sm font-semibold hover:text-white hover:bg-black duration-150">More Detail</button>
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </DoctorLayout>
   );
 };
 
